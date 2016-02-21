306c2bede717bc94be67c1406429da436f616840
mvaude <maxime.vaude@scality.com>
2016-02-21 02:11:17 +0100

    updates over under strategy

--- src/bets/overunder.py
+++ src/bets/overunder.py
@@ -4,23 +4,25 @@ Class for Over Under Bet
   - over(.5)
 """
 from bet import Bet
-from football.game import Game
 
 
 class OverUnder(Bet):
     """
-    >>> [ x for x in OverUnder().over(0.5) ]
+    >>> from football.game import Game
+    >>> [ x for x in OverUnder(Game(3)).over(0.5) ]
     [(0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
-    >>> [ x for x in OverUnder().under(0.5) ]
+    >>> [ x for x in OverUnder(Game(3)).under(0.5) ]
     [(0, 0)]
     """
+    def __init__(self, game):
+        self.game = game
 
     def over(self, goals):
-        for (home, away) in Game(3).score_combinaison():
+        for (home, away) in self.game.score_combinaison():
             if float(home + away) > goals:
                 yield (home, away)
 
     def under(self, goals):
-        for (home, away) in Game(3).score_combinaison():
+        for (home, away) in self.game.score_combinaison():
             if float(home + away) < goals:
                 yield (home, away)
